8d29ebc1474bd728f0ad86cbb223051d78bade9e
Artem <tarachyovaa@mer.ci.nsu.ru>
2026-09-16 23:42:31 +0700

    done

--- lab1/lab1_v2.py
+++ lab1/lab1_v2.py
@@ -5,6 +5,26 @@ import hashlib
 
 def mask(p):
     return hashlib.sha256(p.encode("utf-8")).hexdigest()[:12]
+log_format = "%(asctime)s | [%(levelname)-7s] | %(message)s"
+date_format = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=log_format,
+    datefmt=date_format,
+    force=True,
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler("log/file_txt.log", encoding="utf-8")
+    ]
+)
+logging.info("\n" * 5 )
+logging.info("Логгер успешно сконфигурирован")
+logging.info("Приложение запущено")
+
+res_msg = ""
+res_bool = False
+
+
 try:
     login = input('Email, number or login: ')
     passw = input('Enter your password: ')
@@ -17,71 +37,66 @@ try:
         r"^(?=.*[A-Za-z])(?=.*\d)(?=.*_)[A-Za-z0-9_]+$":'Non correct login (Cyrillic simbols or not numbers or not "_") '
     }
     black_list = ['89537846735', 'artem.tarachev2@gmail.com']
-    l_err = None
-    login_ok = False
-
-    log_format = "%(asctime)s | [%(levelname)-7s] | %(message)s"
-    date_format = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format=log_format,
-        datefmt=date_format,
-        force=True,
-        handlers=[
-            logging.StreamHandler(sys.stdout),
-            logging.FileHandler("log/file_txt.log", encoding="utf-8")
-        ]
-    )
-    logging.info("Логгер успешно сконфигурирован")
-    logging.info("Приложение запущено")
-
+    login_ok = True
 
     if len(login)< 5:
-        l_err = 'Login too short'
-        logging.error(f'Login={login} | error: {l_err}')
+        res_msg = 'Login too short'
+        logging.error(f'Login={login} | error: {res_msg}')
+        login_ok = False
     elif login in black_list:
-        l_err = 'Login is bad(blacklist)'
-        logging.error(f'Login={login} | error: {l_err}')
-    else:
+        res_msg = 'Login is bad(blacklist)'
+        logging.error(f'Login={login} | error: {res_msg}')
+        login_ok = False
+    elif login_ok:
         logging.debug(f'Start login check for format | login={login}')
         for i in patterns.keys():
             # print(f'l: {login},i:  {i }')
             match re.fullmatch(i, login):
                 case re.Match():
                     # print(login)
-                    login_ok = True
                     break
                 case _:
+                    login_ok = False
                     logging.warning(f'Login {login} not matched, because {patterns[i]}')
                     pass
-    if login_ok:
-        logging.info(f'Login successful: {login}')
-    else: logging.error(f'Login failed: {login} | reason: {l_err or "no pattern matched"}')
-    logging.info(f'Start password check | login {login}')
-    if len(passw) < 7:
-        logging.error(f'Login={login} | error: Password too short | pass={mask(passw)}')
+    if not login_ok and res_msg == "":
+        res_msg = 'Login no pattern matched'
+        logging.error(f'Login failed: {login} | error: {res_msg}')
+    elif res_msg != "":
+        res_bool = False
     else:
-        a = b = c = d = err = False
-        for i in passw:
-            if re.fullmatch(r"[А-ЯЁ]", i):
-                a = True
-            elif re.fullmatch(r"[а-яё]", i):
-                b = True
-            elif re.fullmatch(r"\d", i):
-                c = True
-            elif re.fullmatch(r"[^A-Za-z0-9А-Яа-яЁё]", i):
-                d = True
-            else:
-                err = True
-        if err : logging.error(f'Login={login} | Password = {mask(passw)} with Latin letter')
-        elif not a : logging.error(f'Login={login} | Password = {mask(passw)} not big letter')
-        elif not b : logging.error(f'Login={login} | Password = {mask(passw)} not small letter')
-        elif not c : logging.error(f'Login={login} | Password = {mask(passw)} not numbers')
-        elif not d : logging.error(f'Login={login} | Password = {mask(passw)} not special simbols')
-        elif passw != A_passw : logging.error(f'Login={login} | Password = {mask(passw)} Second password not matched')
+        logging.info(f'Login successful: {login}')
+        logging.info(f'Start password check | login {login}')
+        if len(passw) < 7:
+            res_msg = 'Password too short'
+            logging.error(f'Login={login} | error: {res_msg} | pass={mask(passw)}')
         else:
-            logging.info(f'Login={login} | pass={mask(passw)}')
-        print(result_bool)
-        print(result_msg)
+            a = b = c = d = err = False
+            for i in passw:
+                if re.fullmatch(r"[А-ЯЁ]", i):
+                    a = True
+                elif re.fullmatch(r"[а-яё]", i):
+                    b = True
+                elif re.fullmatch(r"\d", i):
+                    c = True
+                elif re.fullmatch(r"[^A-Za-z0-9А-Яа-яЁё]", i):
+                    d = True
+                else:
+                    err = True
+            if err : res_msg = 'Password with Latin letter'
+            elif not a : res_msg = 'Password not big letter'
+            elif not b : res_msg = 'Password  not small letter'
+            elif not c : res_msg = 'Password not numbers'
+            elif not d : res_msg = 'Password not special simbols'
+            elif passw != A_passw : res_msg = 'Second password not matched'
+            if res_msg == '':
+                res_bool = True
+                logging.info(f'Login={login} | pass={mask(passw)} | result: second passw = {mask(passw)} matched')
+            else:
+                logging.error(f'Login {login} | error: {res_msg} | pass={mask(passw)}')
 except Exception:
     logging.exception("Unhandled exception")
+    res_bool = False
+    res_msg = 'Internal error'
+print(res_bool)
+print(res_msg)
